77107e50ab6482f46db9afc632c0ee21b563dee6
TW Udemy Course <tony.ward@vitac.com>
2018-06-18 14:22:16 -0400

    add association between article and user and add debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,9 @@ class ArticlesController < ApplicationController
   end
   
   def create
+    # debugger
     @article = Article.new(article_params)
+    @article.user = User.first
     if (@article.save)
       flash[:success] = "Successfully created"
       redirect_to article_path(@article)
