dd6eb0792a0ef3779fe0f15c840e5e11c8b032df
TW Udemy Course <tony.ward@vitac.com>
2018-06-19 11:41:40 -0400

    create show action and add gravitar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80})
+    gravator_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravator_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")   
+  end
 end
