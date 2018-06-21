635a661603bb03abda3c6255a0c6adc3bfd11a45
TW Udemy Course <tony.ward@vitac.com>
2018-06-21 11:48:52 -0400

    Completed authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
+  
   def index
     @users = User.paginate(page:params[:page], per_page: 5)  
   end
@@ -17,12 +20,10 @@ class UsersController < ApplicationController
     end
   end
   
-  def edit
-    @user = User.find(params[:id])  
+  def edit     
   end
   
   def update 
-    @user = User.find(params[:id]) 
     if @user.update(user_params)
       flash[:success] = "Your account was updated sucessfully"
       redirect_to articles_path      
@@ -31,8 +32,7 @@ class UsersController < ApplicationController
     end
   end
   
-  def show
-    @user = User.find(params[:id]) 
+  def show 
     @user_articles = @user.articles.paginate(page:params[:page], per_page: 5)
   end
   
@@ -40,5 +40,17 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
+  
   
 end
