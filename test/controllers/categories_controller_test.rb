e26ecc69d6c6f222334b1a33f348b59ceab77a1d
TW Udemy Course <tony.ward@vitac.com>
2018-06-26 12:28:41 -0400

    add admin requirement to create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -6,6 +6,7 @@ class CategoriesControllerTest < ActionController::TestCase
   
   def setup
     @category = Category.create(name: "sports") 
+    @user = User.create(username:"john", email: "john@test.com", password: "password", admin: true)
   end
   
   test "should get categories index" do
@@ -14,6 +15,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -21,5 +23,12 @@ class CategoriesControllerTest < ActionController::TestCase
   test "should get show" do
     get(:show, {'id' => @category.id})
     assert_response :success
+  end
+  
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports"}
+    end
+    assert_redirected_to categories_path
   end
 end
